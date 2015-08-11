c9da9733961354d7eef8bfa6d728e170952fdfce
Alexey Debelov <aj001@mail.ru>
2015-08-11 16:46:37 +0500

    Добавлены вкладки настроке АИКов

---
+++ DrillingRig.ConfigApp/AiksSettings/AikSettings.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DrillingRig.ConfigApp.AiksSettings
+{
+	/// <summary>
+	/// Interaction logic for BsEthernetNominalsView.xaml
+	/// </summary>
+	public partial class AikSettings : UserControl
+	{
+		public AikSettings()
+		{
+			InitializeComponent();
+		}
+	}
+}
