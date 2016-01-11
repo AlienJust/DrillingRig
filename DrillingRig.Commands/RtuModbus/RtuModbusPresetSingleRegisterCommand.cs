96e6c9467932fe8a3df419792f65b60ba017e2a8
Alexey Debelov <aj001@mail.ru>
2016-01-11 11:47:00 +0500

    Forgotten comit

---
+++ DrillingRig.Commands/RtuModbus/RtuModbusPresetSingleRegisterCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DrillingRid.Commands.Contracts;
+
+namespace DrillingRig.Commands.RtuModbus
+{
+	public class RtuModbusPresetSingleRegisterCommand : IRrModbusCommandWithReply, IRrModbusCommandResultGetter<IList<byte>>, IRrModbusCommandWithTestReply
+	{
+		private readonly ushort _registerAddress;
+		private readonly ushort _valueToSet;
+
+		public RtuModbusPresetSingleRegisterCommand(ushort registerAddress, ushort valueToSet) {
+			_registerAddress = registerAddress;
+			_valueToSet = valueToSet;
+		}
+
+		public byte CommandCode
+		{
+			get { return 0x06; }
+		}
+
+		public string Name
+		{
+			get { return "Preset Single Register at address: " + _firstRegisterAddress + ", count: " + _registersCountToRead; }
+		}
+
+		public byte[] Serialize() {
+			var result = new byte[4];
+			result[0] = (byte)(_firstRegisterAddress & 0xFF);
+			result[1] = (byte) ((_firstRegisterAddress & 0xFF00) >> 8);
+			return result;
+		}
+
+		public IList<byte> GetResult(byte[] reply) {
+			if (reply.Length != ReplyLength) throw new Exception("wrong reply length");
+			if (reply[0] != ReplyLength - 1) throw new Exception("first byte in reply must have value readed register values bytes count: " + (ReplyLength - 1));
+			return reply.Skip(1).ToList();
+		}
+
+		public int ReplyLength
+		{
+			get {
+				return 1 + _registersCountToRead * 2;
+			}
+		}
+
+		public byte[] GetTestReply() {
+			var rnd = new Random();
+			var testResult = new byte[ReplyLength];
+			rnd.NextBytes(testResult);
+
+			return testResult;
+		}
+	}
+}
---
+++ DrillingRig.Commands/RtuModbus/RtuModbusReadHoldingRegistersCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DrillingRid.Commands.Contracts;
+
+namespace DrillingRig.Commands.RtuModbus
+{
+	public class RtuModbusReadHoldingRegistersCommand : IRrModbusCommandWithReply, IRrModbusCommandResultGetter<IList<byte>>, IRrModbusCommandWithTestReply
+	{
+		private readonly ushort _firstRegisterAddress;
+		private readonly ushort _registersCountToRead;
+
+		public RtuModbusReadHoldingRegistersCommand(ushort firstRegisterAddress, ushort registersCountToRead) {
+			_firstRegisterAddress = firstRegisterAddress;
+			_registersCountToRead = registersCountToRead;
+		}
+
+		public byte CommandCode
+		{
+			get { return 0x03; }
+		}
+
+		public string Name
+		{
+			get { return "Read holding registers Modbus RTU command, first address: " + _firstRegisterAddress + ", count: " + _registersCountToRead; }
+		}
+
+		public byte[] Serialize() {
+			var result = new byte[4];
+			result[0] = (byte)(_firstRegisterAddress & 0xFF);
+			result[1] = (byte) ((_firstRegisterAddress & 0xFF00) >> 8);
+			return result;
+		}
+
+		public IList<byte> GetResult(byte[] reply) {
+			if (reply.Length != ReplyLength) throw new Exception("wrong reply length");
+			if (reply[0] != ReplyLength - 1) throw new Exception("first byte in reply must have value readed register values bytes count: " + (ReplyLength - 1));
+			return reply.Skip(1).ToList();
+		}
+
+		public int ReplyLength
+		{
+			get {
+				return 1 + _registersCountToRead * 2;
+			}
+		}
+
+		public byte[] GetTestReply() {
+			var rnd = new Random();
+			var testResult = new byte[ReplyLength];
+			rnd.NextBytes(testResult);
+
+			return testResult;
+		}
+	}
+}
