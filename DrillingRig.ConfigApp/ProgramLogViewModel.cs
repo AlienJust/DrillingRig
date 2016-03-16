1369704e9bbdfbc9f31440b313b2852dc67a13ed
Alexey Debelov <aj001@mail.ru>
2016-03-16 20:16:55 +0500

    Улучшен лог программы (еще есть баги)

--- DrillingRig.ConfigApp/ProgramLogViewModel.cs
+++ DrillingRig.ConfigApp/ProgramLogViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 using AlienJust.Support.Loggers.Contracts;
 using AlienJust.Support.ModelViewViewModel;
 
@@ -7,14 +8,38 @@ namespace DrillingRig.ConfigApp
 	class ProgramLogViewModel : ViewModelBase, ILogger {
 		private readonly IUserInterfaceRoot _userInterfaceRoot;
 		private readonly ObservableCollection<ILogLine> _logLines;
+		private readonly ICommand _clearLogCmd;
+
+		private bool _scrollAutomaticly;
 
 		public ProgramLogViewModel(IUserInterfaceRoot userInterfaceRoot) {
 			_userInterfaceRoot = userInterfaceRoot;
 			_logLines = new ObservableCollection<ILogLine>();
+
+			_clearLogCmd = new RelayCommand(ClearLog);
+			ScrollAutomaticly = true;
+		}
+
+		private void ClearLog() {
+			_logLines.Clear();
 		}
 
 		public ObservableCollection<ILogLine> LogLines {
 			get { return _logLines; }
+		}
+
+		public ICommand ClearLogCmd {
+			get { return _clearLogCmd; }
+		}
+
+		public bool ScrollAutomaticly {
+			get { return _scrollAutomaticly; }
+			set {
+				if (_scrollAutomaticly != value) {
+					_scrollAutomaticly = value;
+					RaisePropertyChanged(() => ScrollAutomaticly);
+				}
+			}
 		}
 
 
