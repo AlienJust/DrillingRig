44f723b8f7d89f4b7aaab4800c58bc419a0516a2
Alexey Debelov <aj001@mail.ru>
2016-09-14 16:50:53 +0500

    Добавлена заготовка для окна с видом "осцилографа"

---
+++ DrillingRig.ConfigApp/LookedLikeAbb/Oscilloscope/OscilloscopeLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Abt.Controls.SciChart;
+using AlienJust.Support.ModelViewViewModel;
+
+namespace DrillingRig.ConfigApp.LookedLikeAbb.Oscilloscope {
+	class OscilloscopeLogger : ViewModelBase, IParameterLogger {
+		public void LogAnalogueParameter(string parameterName, double? value) {
+			throw new NotImplementedException();
+		}
+
+		public void LogDiscreteParameter(string parameterName, bool? value) {
+			throw new NotImplementedException(); // cannot register, or can?
+		}
+
+		public void RemoveSeries(IChartSeriesViewModel seriesViewModel) {
+			throw new NotImplementedException();
+		}
+	}
+}
