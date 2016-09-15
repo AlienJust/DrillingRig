9158e8f42075b9eca6024dd4d06480923c8ab566
Alexey Debelov <aj001@mail.ru>
2016-09-15 16:17:14 +0500

    Работаю над многопоточностью UI

--- DrillingRig.ConfigApp/App.xaml.cs
+++ DrillingRig.ConfigApp/App.xaml.cs
@@ -1,16 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Windows;
+using System.Windows.Media;
+using AlienJust.Adaptation.WindowsPresentation;
+using DrillingRig.ConfigApp.AinCommand;
+using DrillingRig.ConfigApp.LookedLikeAbb.Chart;
+using DrillingRig.ConfigApp.LookedLikeAbb.Oscilloscope;
 
 namespace DrillingRig.ConfigApp
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
 	public partial class App : Application
 	{
+		private MainViewModel _mainViewModel;
+		private MainWindow _mainWindow;
+		private ManualResetEvent _mainWindowCreationCompleteWaiter;
+
+		private void App_OnStartup(object sender, StartupEventArgs e) {
+			var colors = new List<Color> {
+				Colors.LawnGreen,
+				Colors.Red,
+				Colors.Cyan,
+				Colors.Yellow,
+				Colors.Coral,
+				Colors.LightGreen,
+				Colors.HotPink,
+				Colors.DeepSkyBlue,
+				Colors.Gold,
+				Colors.Orange,
+				Colors.Violet,
+				Colors.White,
+				Colors.Fuchsia,
+				Colors.LightSkyBlue,
+				Colors.LightGray,
+				Colors.Khaki,
+				Colors.SpringGreen,
+				Colors.Tomato,
+				Colors.LightCyan,
+				Colors.Goldenrod,
+				Colors.SlateBlue,
+				Colors.Cornsilk,
+				Colors.MediumPurple,
+				Colors.RoyalBlue,
+				Colors.MediumVioletRed,
+				Colors.MediumTurquoise,
+
+			};
+			_mainWindowCreationCompleteWaiter = new ManualResetEvent(false);
+
+			Thread newWindowThread = new Thread(() => {
+				_mainViewModel = new MainViewModel(new WpfUiNotifierAsync(System.Windows.Threading.Dispatcher.CurrentDispatcher), new WpfWindowSystem(), colors);
+				_mainWindow = new MainWindow { DataContext = _mainViewModel };
+				_mainWindowCreationCompleteWaiter.Set();
+				_mainWindow.Show();
+				
+				System.Windows.Threading.Dispatcher.Run();
+			});
+			newWindowThread.SetApartmentState(ApartmentState.STA);
+			newWindowThread.IsBackground = true;
+			newWindowThread.Start();
+
+			_mainWindowCreationCompleteWaiter.WaitOne();
+
+			var cmdWindow = new CommandWindow(_mainWindow) { DataContext = new CommandWindowViewModel(_mainViewModel.AinCommandAndCommonTelemetryVm) };
+			cmdWindow.Show();
+
+			var chartWindow = new WindowChart(_mainWindow) { DataContext = new WindowChartViewModel(_mainViewModel.ChartControlVm) };
+			chartWindow.Show();
+
+			var oscilloscopeWindow = new OscilloscopeWindow(_mainWindow, colors) { DataContext = new OscilloscopeWindowSciVm() };
+			_mainViewModel.ParamLoggerContainer.AddParamLogger(oscilloscopeWindow);
+
+			oscilloscopeWindow.Show();
+		}
 	}
 }
--- DrillingRig.ConfigApp/MainWindow.xaml.cs
+++ DrillingRig.ConfigApp/MainWindow.xaml.cs
@@ -1,11 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Media;
-using AlienJust.Adaptation.WindowsPresentation;
-using DrillingRig.ConfigApp.AinCommand;
-using DrillingRig.ConfigApp.AinTelemetry;
-using DrillingRig.ConfigApp.LookedLikeAbb.Chart;
-using DrillingRig.ConfigApp.LookedLikeAbb.Oscilloscope;
+﻿using System.Windows;
 using MahApps.Metro.Controls;
 
 namespace DrillingRig.ConfigApp
@@ -18,48 +11,10 @@ namespace DrillingRig.ConfigApp
 		public MainWindow()
 		{
 			InitializeComponent();
-			var colors = new List<Color> {
-				Colors.LawnGreen,
-				Colors.Red,
-				Colors.Cyan,
-				Colors.Yellow,
-				Colors.Coral,
-				Colors.LightGreen,
-				Colors.HotPink,
-				Colors.DeepSkyBlue,
-				Colors.Gold,
-				Colors.Orange,
-				Colors.Violet,
-				Colors.White,
-				Colors.Fuchsia,
-				Colors.LightSkyBlue,
-				Colors.LightGray,
-				Colors.Khaki,
-				Colors.SpringGreen,
-				Colors.Tomato,
-				Colors.LightCyan,
-				Colors.Goldenrod,
-				Colors.SlateBlue,
-				Colors.Cornsilk,
-				Colors.MediumPurple,
-				Colors.RoyalBlue,
-				Colors.MediumVioletRed,
-				Colors.MediumTurquoise,
+		}
 
-			};
-			var mainViewModel = new MainViewModel(new WpfUiNotifierAsync(Dispatcher), new WpfWindowSystem(), colors);
-			DataContext = mainViewModel;
-
-			var cmdWindow = new CommandWindow(this) {DataContext = new CommandWindowViewModel(mainViewModel.AinCommandAndCommonTelemetryVm)};
-			cmdWindow.Show();
-
-			var chartWindow = new WindowChart(this) {DataContext = new WindowChartViewModel(mainViewModel.ChartControlVm)};
-			chartWindow.Show();
-
-			var oscilloscopeWindow = new OscilloscopeWindow(this, colors) { DataContext = new OscilloscopeWindowSciVm()};
-			mainViewModel.ParamLoggerContainer.AddParamLogger(oscilloscopeWindow);
-
-			oscilloscopeWindow.Show();
+		private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+			Application.Current.Shutdown();
 		}
 	}
 }
