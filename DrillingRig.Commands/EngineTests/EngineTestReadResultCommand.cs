815d38fa2c6eb8a41084c62b62510c62fd2b0586
Alexey Debelov <aj001@mail.ru>
2016-12-26 08:23:05 +0500

    Подготовка к реализации чтения результатов тестирования двигателя

---
+++ DrillingRig.Commands/EngineTests/EngineTestReadResultCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DrillingRid.Commands.Contracts;
+
+namespace DrillingRig.Commands.EngineTests {
+	public class EngineTestReadResultCommand : IRrModbusCommandWithReply, IRrModbusCommandResultGetter<IEngineTestResult>, IRrModbusCommandWithTestReply {
+
+		public EngineTestReadResultCommand() {
+
+		}
+
+		public byte CommandCode => 0x93;
+
+		public string Name => "Запуск теста двигателя";
+
+		public byte[] Serialize()
+		{
+			return new byte[0];
+		}
+
+		byte[] GetBytesLe(int value) {
+			var bytes = BitConverter.GetBytes(value);
+			if (BitConverter.IsLittleEndian) return bytes;
+			return bytes.Reverse().ToArray(); // TODO: improve perfomance
+		}
+		byte[] GetBytesLe(float value) {
+			var bytes = BitConverter.GetBytes(value);
+			if (BitConverter.IsLittleEndian) return bytes;
+			return bytes.Reverse().ToArray();
+		}
+
+		/// <summary>
+		/// Получает результат выполнения команды
+		/// </summary>
+		/// <param name="reply">Байты ответа</param>
+		/// <returns>Успешность запуска тестирования</returns>
+		public bool GetResult(byte[] reply) {
+			if (reply.Length != ReplyLength)
+				throw new Exception("Reply error, reply length must be 1");
+			return reply[0] == 0;
+		}
+
+		public int ReplyLength => 1;
+
+		public byte[] GetTestReply() {
+			return new byte[0];
+		}
+	}
+}
